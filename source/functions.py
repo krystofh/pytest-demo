683de4797052ed954aa116092ce7ce134b5c572e
Krystof Hes <krystof.hes@seznam.cz>
2024-12-02 12:43:44 +0100

    Add basic functions
    Initial commit containing basic files

---
+++ source/functions.py
@@ -0,0 +1,30 @@
+# Functions that shall be tested using pytest
+# Date: 02.12.2024
+# Author: krystofh
+
+
+def add(a: int, b: int) -> int:
+    result = a + b
+    print(f"{a} + {b} = {result}")
+    return result
+
+
+def subtract(a: int, b: int) -> int:
+    result = a - b
+    print(f"{a} - {b} = {result}")
+    return result
+
+
+def multiply(a: int, b: int) -> int:
+    result = a * b
+    print(f"{a} * {b} = {result}")
+    return result
+
+
+def divide(a: int, b: int) -> None | float:
+    if b == 0:
+        print("Division by zero is not allowed.")
+        return
+    result = a / b
+    print(f"{a} / {b} = {result}")
+    return result
---
+++ source/main.py
@@ -0,0 +1,5 @@
+from functions import add, subtract, multiply, divide
+
+if __name__ == "__main__":
+    print("Hi")
+    add(1, 2)
